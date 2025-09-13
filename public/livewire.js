41e5f26c10777c861ee130aca7f401ca3b73b946
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-13 21:34:40 +0300

    refactor(livewire): restructure snapshot handling and request logic for improved clarity

--- public/livewire.js
+++ public/livewire.js
@@ -1,17 +1,30 @@
 document.querySelectorAll("[wire\\:snapshot]").forEach((el) => {
-    let snapshot = JSON.parse(el.getAttribute("wire:snapshot"));
+    el.__livewire = JSON.parse(el.getAttribute("wire:snapshot"));
+    initWireClick(el);
+});
+function initWireClick(el) {
     el.addEventListener("click", (e) => {
         if (!e.target.hasAttribute("wire:click")) return;
         let method = e.target.getAttribute("wire:click");
-        fetch("/api/live", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                snapshot: snapshot,
-                callMethod: method,
-            }),
-        });
+        sendRequest(el, { callMethod: method });
     });
-});
+}
+function sendRequest(el, addToPayload) {
+    let snapshot = el.__livewire;
+    fetch("/api/live", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            snapshot: snapshot,
+            ...addToPayload,
+        }),
+    })
+        .then((i) => i.json())
+        .then((response) => {
+            let { html, snapshot } = response;
+            el.__livewire = snapshot;
+            el.innerHTML = html;
+        });
+}
