99153ce06c31d9134f926e730099a1a4c911dd6d
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-15 23:28:15 +0300

    feat(todo): connect add button - Handle wire:click for todo creation

--- app/Http/Live/Todo.php
+++ app/Http/Live/Todo.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Live;
 
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Log;
+
 class Todo {
-    public string $draft = '';
+    public ?string $draft = '';
     /** @var string[] */
-    public array $todos = ['Study livewire', 'Workout', 'Study AWS fundamentals'];
+    public array $todos = [];
     public function addTodo() {
-        if ($this->draft === '') return;
+        if (!$this->draft) {
+            $this->todos[] = Inspiring::quote();
+            return;
+        }
         $this->todos[] = $this->draft;
         $this->draft = '';
     }
@@ -16,11 +22,11 @@ class Todo {
         <div class="flex flex-col items-center w-full">
             <div class="flex space-x-2">
                 <input type="text" wire:model="draft" />
-                <button>+ Todo</button>
+                <button wire:click="addTodo">+ Todo</button>
             </div>
             <ul class="flex flex-col items-center space-y-1">
                 @foreach($todos as $todo)
-                <li class="p-2 duration-1000 border rounded-lg shadow-lg min-w-md hover:translate-y-1 hover:scale-105 hover:shadow-2xl hover:mb-2">{{$todo}}</li>
+                <li class="p-2 duration-1000 border rounded-lg shadow-lg min-w-md hover:translate-y-1 hover:scale-105 hover:shadow-2xl hover:mb-2">{!! $todo !!}</li>
                 @endforeach
             </ul>
         </div>
