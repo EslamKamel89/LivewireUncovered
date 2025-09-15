5294bdd3797c8fcbcb9c719c6fb4805447918d07
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-15 22:53:50 +0300

    feat(ui): implement todo component UI

--- app/Http/Live/Todo.php
+++ app/Http/Live/Todo.php
@@ -5,7 +5,15 @@ namespace App\Http\Live;
 class Todo {
     public function render() {
         return <<<'HTML'
-        <div>hello world</div>
+        <div class="flex flex-col items-center w-full">
+            <div class="flex space-x-2">
+                <input type="text" />
+                <button>+ Todo</button>
+            </div>
+            <ul class="flex flex-col items-center">
+                <li class="p-2 border rounded-lg shadow-lg min-w-md">one todo</li>
+            </ul>
+        </div>
         HTML;
     }
 }
