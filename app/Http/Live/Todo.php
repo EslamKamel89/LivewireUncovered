dfdd4d602a01c2312b238cc45eb1e87768945ba6
EslamKamel89 <elsamkamelforex@gmail.com>
2025-09-15 23:06:17 +0300

    feat(todo): implement core functionality - Add wire:model binding - Create $draft and $todos properties - Implement addTodo method

--- app/Http/Live/Todo.php
+++ app/Http/Live/Todo.php
@@ -3,15 +3,25 @@
 namespace App\Http\Live;
 
 class Todo {
+    public string $draft = '';
+    /** @var string[] */
+    public array $todos = ['Study livewire', 'Workout', 'Study AWS fundamentals'];
+    public function addTodo() {
+        if ($this->draft === '') return;
+        $this->todos[] = $this->draft;
+        $this->draft = '';
+    }
     public function render() {
         return <<<'HTML'
         <div class="flex flex-col items-center w-full">
             <div class="flex space-x-2">
-                <input type="text" />
+                <input type="text" wire:model="draft" />
                 <button>+ Todo</button>
             </div>
-            <ul class="flex flex-col items-center">
-                <li class="p-2 border rounded-lg shadow-lg min-w-md">one todo</li>
+            <ul class="flex flex-col items-center space-y-1">
+                @foreach($todos as $todo)
+                <li class="p-2 duration-1000 border rounded-lg shadow-lg min-w-md hover:translate-y-1 hover:scale-105 hover:shadow-2xl hover:mb-2">{{$todo}}</li>
+                @endforeach
             </ul>
         </div>
         HTML;
